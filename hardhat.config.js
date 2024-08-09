7206a60488a19b9298bb7757ec6c89f03ae2fe08
MrBeen01 <162284772+MrBeen01@users.noreply.github.com>
2024-08-09 12:35:39 +0530

    Add files via upload
    project created

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+const NEXT_PUBLIC_POLYGON_MUMBAI_RPC = "https://rpc-amoy.polygon.technology/";
+const NEXT_PUBLIC_PRIVATE_KEY = "b3d9d375c4b8436122b4dffcaf4f1e8f80675696b4e89e62cc88dcf23a2b4834";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    polygon_amoy: {
+      url: NEXT_PUBLIC_POLYGON_MUMBAI_RPC,
+      accounts: [`0x${NEXT_PUBLIC_PRIVATE_KEY}`],
+    },
+  },
+};
